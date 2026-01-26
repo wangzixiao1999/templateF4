34e6bc5a261a457bf22006f1fa8f6971bc640b1e
子潇 王 <815063236@qq.com>
2026-01-26 15:18:22 +0800

    yt8512c芯片udp调通

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -57,6 +57,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define ETH_RESET_Pin GPIO_PIN_2
+#define ETH_RESET_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
